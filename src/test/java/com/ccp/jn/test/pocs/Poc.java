c4b1020b2f2d6719bb104d46202d1a3a90245694
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-03-07 23:04:16 -0300

    .

--- src/test/java/com/ccp/jn/test/pocs/Poc.java
+++ src/test/java/com/ccp/jn/test/pocs/Poc.java
@@ -1,9 +1,16 @@
 package com.ccp.jn.test.pocs;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Poc {
 	public static void main(String[] args) {
-		int x = 86_400_000;
-		System.out.println(x);
+		List<String> arrayList = Arrays.asList();
+		Stream<String> stream = arrayList.stream();
+		List<String> collect = stream.collect(Collectors.toList());
+		List<String> collect2 = stream.collect(Collectors.toList());
 	}
 }
 record Pessoa(int idade, String nome) {
