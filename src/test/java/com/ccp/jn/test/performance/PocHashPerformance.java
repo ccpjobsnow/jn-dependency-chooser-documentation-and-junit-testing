95533c3f35f282c34dc8523a73f41313663eb8c9
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-30 01:45:48 -0300

    VisAsyncBusinessPositionSave

--- src/test/java/com/ccp/jn/test/performance/PocHashPerformance.java
+++ src/test/java/com/ccp/jn/test/performance/PocHashPerformance.java
@@ -7,16 +7,31 @@ import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.especifications.db.utils.CcpDbRequester;
 import com.ccp.especifications.http.CcpHttpRequester;
 import com.ccp.especifications.http.CcpHttpResponse;
+import com.ccp.implementations.db.bulk.elasticsearch.CcpElasticSerchDbBulk;
+import com.ccp.implementations.db.crud.elasticsearch.CcpElasticSearchCrud;
 import com.ccp.implementations.db.utils.elasticsearch.CcpElasticSearchDbRequest;
 import com.ccp.implementations.http.apache.mime.CcpApacheMimeHttp;
 import com.ccp.implementations.json.gson.CcpGsonJsonHandler;
+import com.ccp.implementations.password.mindrot.CcpMindrotPasswordHandler;
+import com.jn.commons.entities.JnEntityInstantMessengerMessageSent;
 
 public class PocHashPerformance {
 	static{
 		CcpDependencyInjection.loadAllDependencies(new CcpApacheMimeHttp(), new CcpGsonJsonHandler(), new CcpElasticSearchDbRequest());
 	}
 	public static void main(String[] args) {
-		
+		CcpDependencyInjection.loadAllDependencies(new CcpGsonJsonHandler(), new CcpElasticSearchCrud(),
+				new CcpElasticSearchDbRequest(), new CcpApacheMimeHttp(), new CcpMindrotPasswordHandler(),
+				new CcpElasticSerchDbBulk());
+
+		boolean exists = JnEntityInstantMessengerMessageSent.INSTANCE.exists(new CcpJsonRepresentation("{\r\n"
+				+ "                        \"recipient\": 751717896,\r\n"
+				+ "                        \"interval\": 24805,\r\n"
+				+ "                        \"message\": \"org.springframework.web.HttpRequestMethodNotSupportedException\\n\\n[RequestMappingInfoHandlerMapping.handleNoMatch:265\\n, AbstractHandlerMethodMapping.lookupHandlerMethod:441\\n, AbstractHandlerMethodMapping.getHandlerInternal:382\\n, RequestMappingInfoHandlerMapping.getHandlerInternal:126\\n, RequestMappingInfoHandlerMapping.getHandlerInternal:68\\n, AbstractHandlerMapping.getHandler:507\\n, DispatcherServlet.getHandler:1283\\n, DispatcherServlet.doDispatch:1065\\n, DispatcherServlet.doService:979\\n, FrameworkServlet.processRequest:1014\\n, FrameworkServlet.doGet:903\\n, HttpServlet.service:527\\n, FrameworkServlet.service:885\\n, HttpServlet.service:614\\n, ServletHolder.handle:736\\n, ServletHandler.doFilter:1614\\n, WebSocketUpgradeFilter.doFilter:195\\n, FilterHolder.doFilter:205\\n, ServletHandler.doFilter:1586\\n, CcpPutSessionValuesAndExecuteTaskFilter.doFilter:52\\n, FilterHolder.doFilter:205\\n, ServletHandler.doFilter:1586\\n, CcpValidEmailFilter.doFilter:54\\n, FilterHolder.doFilter:205\\n, ServletHandler.doFilter:1586\\n, ServerHttpObservationFilter.doFilterInternal:109\\n, OncePerRequestFilter.doFilter:116\\n, FilterHolder.doFilter:205\\n, ServletHandler.doFilter:1586\\n, CharacterEncodingFilter.doFilterInternal:201\\n, OncePerRequestFilter.doFilter:116\\n, FilterHolder.doFilter:205\\n, ServletHandler.doFilter:1586\\n, ServletHandler.handle:1547\\n, ServletChannel.dispatch:797\\n, ServletChannel.handle:428\\n, ServletHandler.handle:464\\n, SecurityHandler.handle:571\\n, SessionHandler.handle:703\\n, ContextHandler.handle:761\\n, Server.handle:179\\n, HttpChannelState.run:594\\n, HttpConnection.onFillable:424\\n, AbstractConnection.succeeded:322\\n, FillInterest.fillable:99\\n, SelectableChannelEndPoint.run:53\\n, AdaptiveExecutionStrategy.runTask:478\\n, AdaptiveExecutionStrategy.consumeTask:441\\n, AdaptiveExecutionStrategy.tryProduce:293\\n, AdaptiveExecutionStrategy.produce:195\\n, QueuedThreadPool.runJob:971\\n, QueuedThreadPool.doRunJob:1201\\n, QueuedThreadPool.run:1156\\n, Thread.run:842\\n]\\n\\n\\nRequest method 'GET' is not supported\\n\\nCaused by:\\n{}\",\r\n"
+				+ "                        \"subjectType\": \"notifyError\",\r\n"
+				+ "                        \"token\": \"1122898762:AAGE69KRQ0P0TBZNPXgAMDqNaJCd7jFP_G0\"\r\n"
+				+ "                    }"));
+		System.out.println(exists);
 		
 	}
 	
