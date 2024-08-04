f3f4ab0d2a728282d7353572a7cff1fa84192693
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-08-04 19:32:27 -0300

    .

--- src/test/java/com/ccp/jn/test/pocs/Poc.java
+++ src/test/java/com/ccp/jn/test/pocs/Poc.java
@@ -1,8 +1,13 @@
 package com.ccp.jn.test.pocs;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.ccp.constantes.CcpConstants;
+import com.ccp.decorators.CcpCollectionDecorator;
 import com.ccp.decorators.CcpFileDecorator;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.decorators.CcpStringDecorator;
@@ -12,6 +17,7 @@ import com.ccp.especifications.db.crud.CcpCrud;
 import com.ccp.especifications.db.crud.CcpSelectUnionAll;
 import com.ccp.especifications.db.query.CcpDbQueryOptions;
 import com.ccp.especifications.db.query.CcpQueryExecutor;
+import com.ccp.especifications.db.utils.CcpEntityField;
 import com.ccp.especifications.http.CcpHttpRequester;
 import com.ccp.especifications.http.CcpHttpResponse;
 import com.ccp.implementations.db.crud.elasticsearch.CcpElasticSearchCrud;
@@ -37,16 +43,193 @@ public class Poc {
 	}
 	
 	public static void main(String[] args) {
-		String extractStringContent = new CcpStringDecorator("documentation\\rhs.txt").file().extractStringContent();
-		Set<String> extractFromText = new CcpStringDecorator(extractStringContent).email().extractFromText("\\/|\\s|\n|\\:|\\,|\\;|\\!|\\?|\\[|\\]|\\{|\\}|\\<|\\>|\\=|\\(|\\)\\ |\\'|\\\"|\\`");
-		for (String string : extractFromText) {
-			System.out.println(string);
+		CcpFileDecorator vagasFile = new CcpStringDecorator("c:\\logs\\vagas.json").file();
+		List<CcpJsonRepresentation> vagas = vagasFile.asJsonList();
+	
+	}
+
+	static void criarArquivoDeVagas() {
+		CcpDbQueryOptions queryMatchAll = 
+				CcpDbQueryOptions.INSTANCE
+					.matchAll()
+				;
+		Set<Object> emailsDasVisualizacoes = getEmails(queryMatchAll, "visualizacao_de_curriculo", "email");
+		Set<Object> emailsDasVagas = getEmails(queryMatchAll, "vagas", "email", "mail");
+		
+		List<Object> intersectList = new CcpCollectionDecorator(emailsDasVisualizacoes).getIntersectList(emailsDasVagas);
+		
+		CcpJsonRepresentation mgetJson = CcpConstants.EMPTY_JSON;
+		CcpEntityField idField = new CcpEntityField() {
+			public String name() {
+				return "email";
+			}
+			public boolean isPrimaryKey() {
+				return false;
+			}
+		};
+	
+		CcpDbQueryOptions queryToSearchViews = CcpDbQueryOptions.INSTANCE.startSimplifiedQuery().terms(idField, intersectList).endSimplifiedQueryAndBackToRequest();
+		Set<Object> candidatos = getEmails(queryToSearchViews, "visualizacao_de_curriculo", "candidato", "candidate");
+		CcpJsonRepresentation template = new CcpJsonRepresentation("{\r\n"
+				+ "    \"_index\": \"profissionais2\",\r\n"
+				+ "    \"_source\": {\r\n"
+				+ "        \"include\": [\r\n"
+				+ "            \"curriculo.arquivo\",\r\n"
+				+ "            \"pretensaoPj\",\r\n"
+				+ "            \"pretensaoClt\",\r\n"
+				+ "            \"pcd\",\r\n"
+				+ "            \"mudanca\",\r\n"
+				+ "            \"homeoffice\",\r\n"
+				+ "            \"experiencia\",\r\n"
+				+ "            \"disponibilidade\",\r\n"
+				+ "            \"ddd\",\r\n"
+				+ "            \"bitcoin\"\r\n"
+				+ "        ]\r\n"
+				+ "    }\r\n"
+				+ "}");
+		for (Object candidato : candidatos) {
+			CcpJsonRepresentation doc = template.put("_id", candidato);
+			mgetJson = mgetJson.addToList("docs", doc);
 		}
 		
-		String body = CcpConstants.EMPTY_JSON.asPrettyJson();
-		CcpHttpResponse executeHttpRequest = CcpDependencyInjection.getDependency(CcpHttpRequester.class).executeHttpRequest("http://localhost:9200/profissionais2/_doc/lucascavalcantedeo@gmail.com", "POST", CcpConstants.EMPTY_JSON, body);
-		System.out.println(executeHttpRequest);
+		CcpHttpResponse executeHttpRequest = CcpDependencyInjection.getDependency(CcpHttpRequester.class).executeHttpRequest("http://localhost:9200/_mget", "POST", CcpConstants.EMPTY_JSON, mgetJson.asUgglyJson());
+		CcpJsonRepresentation asSingleJson = executeHttpRequest.asSingleJson();
+		
+		List<CcpJsonRepresentation> collect = asSingleJson.getAsJsonList("docs").stream().map(json -> {
+			String id = json.getAsString("_id");
+			CcpJsonRepresentation source = json.getInnerJson("_source");
+			CcpJsonRepresentation put = source.put("id", id);
+			return put;
+		}).collect(Collectors.toList());
+		
+		CcpJsonRepresentation resumes = CcpConstants.EMPTY_JSON;
+		
+		for (CcpJsonRepresentation curriculo : collect) {
+			String id = curriculo.getAsString("id");
+			resumes = resumes.put(id, curriculo);
+		}
+		
+		
+		CcpJsonRepresentation candidatosAgrupadosPorRecrutadores = getCandidatosAgrupadosPorRecrutadores(queryMatchAll);
+		CcpJsonRepresentation vagasAgrupadosPorRecrutadores = getVagasAgrupadosPorRecrutadores(intersectList);
+		Set<String> recrutadores = vagasAgrupadosPorRecrutadores.fieldSet();
+		List<CcpJsonRepresentation> todasAsVagas = new ArrayList<>();
+		CcpJsonRepresentation res = new CcpJsonRepresentation(resumes.content);
+		for (String recrutador : recrutadores) {
+			List<CcpJsonRepresentation> curriculos = candidatosAgrupadosPorRecrutadores.getAsStringList(recrutador)
+					.stream().map(x -> res.getInnerJson(x)).collect(Collectors.toList())
+					;
 
+			List<CcpJsonRepresentation> vagas = vagasAgrupadosPorRecrutadores.getAsJsonList(recrutador);
+
+			int k = 0;
+			for (CcpJsonRepresentation curriculo : curriculos) {
+				CcpJsonRepresentation vaga = vagas.get(k++ % vagas.size());
+				CcpJsonRepresentation putAll = vaga.putAll(curriculo);
+				todasAsVagas.add(putAll);
+			}
+		}
+		CcpFileDecorator vagasFile = new CcpStringDecorator("c:\\logs\\vagas.json").file().reset();
+		vagasFile.append(todasAsVagas.toString());
+	}
+	
+	static class AgruparVagasPorRecrutadores implements java.util.function.Consumer<CcpJsonRepresentation>{
+		
+		CcpJsonRepresentation vagasAgrupadasPorRecrutadores = CcpConstants.EMPTY_JSON;
+		
+		public void accept(CcpJsonRepresentation json) {
+			
+			String recrutador = json.getAsObject("mail");
+			String contato = json.getAsString("contato");
+			String texto = json.getAsString("vaga");
+			String contactChannel =  new CcpStringDecorator(contato.trim()).email().isValid() ? "email" : "link";
+			
+			CcpJsonRepresentation vaga = 
+			CcpConstants.EMPTY_JSON
+			.put("channel", contato)
+			.put("email", recrutador)
+			.put("description", texto)
+			.put("contactChannel", contactChannel);
+			
+			this.vagasAgrupadasPorRecrutadores = this.vagasAgrupadasPorRecrutadores.addToList(recrutador, vaga);
+			
+		}
+		
+	}
+	
+	static class AgruparCandidatosPorRecrutadores implements java.util.function.Consumer<CcpJsonRepresentation>{
+		
+		CcpJsonRepresentation candidatosAgrupadosPorRecrutadores = CcpConstants.EMPTY_JSON;
+		
+		public void accept(CcpJsonRepresentation json) {
+			String candidato = json.getAsObject("candidate", "candidato");
+			String recrutador = json.getAsString("email");
+			this.candidatosAgrupadosPorRecrutadores = this.candidatosAgrupadosPorRecrutadores.addToList(recrutador, candidato);
+			
+		}
+		
+	}
+
+	static CcpJsonRepresentation getVagasAgrupadosPorRecrutadores(List<Object> intersectList) {
+		CcpEntityField idField = new CcpEntityField() {
+			public String name() {
+				return "mail";
+			}
+			public boolean isPrimaryKey() {
+				return false;
+			}
+		};
+		CcpDbQueryOptions query = CcpDbQueryOptions.INSTANCE.startSimplifiedQuery().terms(idField, intersectList).endSimplifiedQueryAndBackToRequest();
+
+		String[] resourcesNames = new String[] {"vagas"};
+		CcpQueryExecutor queryExecutor = CcpDependencyInjection.getDependency(CcpQueryExecutor.class);
+		AgruparVagasPorRecrutadores consumer = new AgruparVagasPorRecrutadores();
+		queryExecutor.consumeQueryResult(
+				query, 
+				resourcesNames, 
+				"10s", 
+				10000, 
+				consumer, "contato", "vaga", "mail");
+		return consumer.vagasAgrupadasPorRecrutadores;
+	}
+
+	static CcpJsonRepresentation getCandidatosAgrupadosPorRecrutadores(CcpDbQueryOptions query) {
+		
+		String[] resourcesNames = new String[] {"visualizacao_de_curriculo"};
+		CcpQueryExecutor queryExecutor = CcpDependencyInjection.getDependency(CcpQueryExecutor.class);
+		AgruparCandidatosPorRecrutadores consumer = new AgruparCandidatosPorRecrutadores();
+		queryExecutor.consumeQueryResult(
+				query, 
+				resourcesNames, 
+				"10s", 
+				10000, 
+				consumer, "candidate", "candidato", "email");
+		return consumer.candidatosAgrupadosPorRecrutadores;
+	}
+	
+	
+	static Set<Object> getEmails(CcpDbQueryOptions query, String tabela, String... fields) {
+		String[] resourcesNames = new String[] {tabela};
+		CcpQueryExecutor queryExecutor = CcpDependencyInjection.getDependency(CcpQueryExecutor.class);
+		Set<Object> set = new HashSet<>();
+		queryExecutor.consumeQueryResult(
+				query, 
+				resourcesNames, 
+				"10s", 
+				10000, 
+				json -> {
+					for (String field : fields) {
+						
+						String value = json.getAsTextDecorator(field).content.trim().toLowerCase();
+						
+						if(value.isEmpty()) {
+							continue;
+						}
+						set.add(value);
+					}
+					
+				}, fields);
+		return set;
 	}
 
 	static void excluirCurriculo() {
@@ -153,7 +336,7 @@ public class Poc {
 		queryExecutor.consumeQueryResult(
 				queryToSearchLastUpdatedResumes, 
 				resourcesNames, 
-				"10m", 
+				"10s", 
 				10000, 
 				vaga -> {
 					String texto = vaga.getAsString("vaga").replace("\n", "").trim();
